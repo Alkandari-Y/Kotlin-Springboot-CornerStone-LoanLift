b41491f25213f26371f0a1d972719e85ddbb6d3c
Yousef <yayalkandari@gmail.com>
2025-05-09 21:59:04 +0300

    sample data

---
+++ banking/src/main/resources/db/sample.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.categories (name)
+VALUES
+    ('health'),
+    ('personal'),
+    ('retails'),
+    ('fitness'),
+    ('luxury'),
+    ('education'),
+    ('utilities'),
+    ('electronics');
+
