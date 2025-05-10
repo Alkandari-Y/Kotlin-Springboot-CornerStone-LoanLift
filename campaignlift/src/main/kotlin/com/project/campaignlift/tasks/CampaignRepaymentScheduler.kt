199ea41ca2ae33e6d9b6ee234424bbbff825f608
Alkandari-Y <56042101+Alkandari-Y@users.noreply.github.com>
2025-05-10 13:58:49 +0300

    schedular

--- campaignlift/src/main/kotlin/com/project/campaignlift/tasks/CampaignRepaymentScheduler.kt
+++ campaignlift/src/main/kotlin/com/project/campaignlift/tasks/CampaignRepaymentScheduler.kt
@@ -9,11 +9,11 @@ class CampaignRepaymentScheduler(
     private val repaymentService: RepaymentService
 ) {
 
-    // Runs at 2:00 AM on the 22nd day of every month (UTC time)
+    // Runs at 2:00 AM on the 22nd day of every month (Asia/Kuwait time)
     // Second Minute Hours DayOfMonth Month DayOfWeek
     //   0      0       0       0       0       ?
-//    @Scheduled(cron = "0 30 14 13 5 ?", zone = "UTC") // for demo
-    @Scheduled(cron = "0 0 2 22 * ?", zone = "UTC")
+//    @Scheduled(cron = "0 30 14 13 5 ?", zone = "Asia/Kuwait") // for demo
+    @Scheduled(cron = "0 0 11 22 * ?", zone = "Asia/Kuwait")
     fun processMonthlyRepayments() {
         println("[Scheduler] Running monthly repayment")
         repaymentService.processMonthlyRepayments()
